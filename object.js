5205bd2c6a6945f59a25800b2deeaea9fbf52cd8
2azahar786 <2azahar786@gmail.com>
2023-01-14 23:25:05 +0530

    second commit

--- object.js
+++ object.js
@@ -16,7 +16,7 @@ const obj1={
 obj1.fuel=200
 obj1['year']=2000
 
-console.log(obj1);
+// console.log(obj1);
 
 // console.log(object);
 
@@ -41,3 +41,40 @@ const obj3=object.create(power)
 // console.log(obj3);
 // console.log(obj3.cordinate);
 // console.log(object.getprototypeof(obj3));
+
+// part-4
+
+const obj4 =object.create({})
+object.defineProperty(obj4, 'book', {
+    icon:'📘',
+    get: () => '📘',
+    enumerable: true
+})
+
+// console.log(obj4);
+// console.log(obj4.book);
+// console.log(object.getPrototypeof(obj4));
+
+for (k in obj4){
+    // console.log("Value is k:", K);
+}
+
+// Part-5
+
+const obj5= {
+    comics:'marvel',
+    pen:'',
+    printComic :function(){
+        this.pen += '🖊️🗒️'
+        console.log(this);
+        return this;
+    }
+
+    printaComic: () =>{
+        this.pen :'🖊️🗒️'
+        console.log(this);
+    },
+}
+
+console.log(obj5.printcomic().printComic().printComic);
+console.log(obj5.printaComic());
---
+++ objects.js
@@ -0,0 +1,5 @@
+const power={
+ fly:true,
+ cordinate: math.random() + 2
+}
+ console.log(power);
